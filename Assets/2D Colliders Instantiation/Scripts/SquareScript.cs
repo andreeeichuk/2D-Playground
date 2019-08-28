359cbb6e637797f72d6c0a38dc0dfbbe481749e1
Ivan Andreichuk <andreeeichuk@gmail.com>
2019-08-28 18:06:12 +0300

    Add Physics2D.Overlapcircle experiment

---
+++ Assets/2D Colliders Instantiation/Scripts/SquareScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SquareScript : MonoBehaviour
+{
+    public Collider2D myCollider;
+
+    private void Awake()
+    {
+        CountColliders();
+    }
+
+    public void CountColliders()
+    {
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, 100f);
+
+        Debug.Log("Colliders inside my circle:" + colliders.Length);
+    }
+}
---
+++ Assets/2D Colliders Instantiation/Scripts/SquareSpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SquareSpawner : MonoBehaviour
+{
+    public GameObject squarePrefab;
+
+    private Vector2 maxPoints = new Vector2(20,20);
+    private Vector2 minPoints = new Vector2(-20,-20);
+
+    private int checkedSquare = 9;
+
+    private void Start()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            SquareScript square = SpawnSquare();
+            //if (i == checkedSquare)
+            //    square.CountColliders();
+        }
+    }
+
+    private SquareScript SpawnSquare()
+    {
+        float randomX = Random.Range(minPoints.x, maxPoints.x);
+        float randomY = Random.Range(minPoints.y, maxPoints.y);
+
+        GameObject square = Instantiate(squarePrefab, new Vector3(randomX, randomY), Quaternion.identity);
+
+        return square.GetComponent<SquareScript>();
+    }
+}
